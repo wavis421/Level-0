b2f846cd1402f02550fb21c546176617b93bc39a
wavis421 <wendy.avis@jointheleague.org>
2017-08-06 19:48:26 -0700

    Misc recipe updates

---
+++ src/AnimalFarm.java
@@ -0,0 +1,62 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class AnimalFarm {
+
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that
+		 * animal.
+		 */
+
+		/* 2. Make it so that the user can keep entering new animals. */
+		playMoo();
+		playQuack();
+		playWoof();
+	}
+
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	  String quackFile = "/Users/League/Google Drive/league-sounds/quack.wav";
+	String mooFile = "/Users/League/Google Drive/league-sounds/moo.wav";
+	String woofFile = "/Users/League/Google Drive/league-sounds/woof.wav";
+	String meowFile = "/Users/League/Google Drive/league-sounds/meow.wav";
+	String llamaFile = "/Users/League/Google Drive/league-sounds/llama.wav";
+
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+    	try {
+   		 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundFile));
+   		 Clip clip = AudioSystem.getClip();
+   		 clip.open(audioInputStream);
+   		 clip.start();
+   		 Thread.sleep(3400);
+    	} catch (Exception ex) {
+        	ex.printStackTrace();
+    	}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
+
+}
+
+
+
